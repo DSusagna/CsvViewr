b90c51da12ee935bdd8a74f0b8dcdff896cee3e3
DSusagna <david.susagna.h@gmail.com>
2024-03-16 19:54:31 +0100

    update .bat

--- runWindows.bat
+++ runWindows.bat
@@ -1,21 +1,10 @@
 @echo off
 
-REM Función para verificar si el JDK está instalado
-:check_java
-java -version > nul 2>&1
-if %errorlevel% neq 0 (
-    goto :install_java
-) else (
-    echo JDK está instalado.
-)
-
-REM Función para instalar el JDK utilizando Chocolatey (opcional, si tienes Chocolatey instalado)
-:install_java
-echo JDK no está instalado. Instalando JDK...
-REM Coloca aquí los comandos de instalación del JDK utilizando Chocolatey
-goto :run_jar
-
-REM Ejecutar el archivo .jar
 :run_jar
-echo Ejecutando CsvViewr.jar...
-java -jar "%~dp0artifacts\CsvViewr.jar"
+echo Running CsvViewr.jar...
+echo.
+cd artifacts
+java -jar CsvViewr.jar
+cd ..
+
+
